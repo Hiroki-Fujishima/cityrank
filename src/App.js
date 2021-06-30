874fb36c31e95d21efad14acba84d5b70e5b62a1
Hiroki-Fujishima <ucuc.as@gmail.com>
2021-07-01 02:17:08 +0900

    fix_map_temple

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-//issue1
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React, {Component} from 'react';
+import {DataSearch,ReactiveBase,ReactiveList,ResultList,SelectedFilters} from '@appbaseio/reactivesearch'; // eslint-disable-line
+
+const {ResultListWrapper} = ReactiveList;
+
+class App extends Component {
+    render() {
+        return ( 
+            <div>
+                <ReactiveBase
+                    app = "cityindex"
+                    url = "https://search-hiroki-app3-ulccwx5iv6spzusktmiqxv7pjy.ap-northeast-1.es.amazonaws.com/"
+                >
+                    <DataSearch
+                        componentId = "search-component"
+                        dataField = {["countryname"]}
+                        queryFormat = "and"
+                    />
+                    <ReactiveList
+                        componentId = "list-component"
+                        pagination = {true}
+                        size = {10}
+                        react = {{
+                            "and": ["search-component"]
+                        }}
+                    >
+                        {({data, error, loading}) => (
+                            <ResultListWrapper>
+                                {
+                                    data.map(item => (
+                                        <ResultList key = {item._id}>
+                                            <ResultList.Content>
+                                                <ResultList.Title
+                                                    dangerouslySetInnerHTML = {{
+                                                        __html: item.cityname
+                                                    }}
+                                                />
+                                                <ResultList.Description>
+                                                    <div> {item.countryname} </div>
+                                                    <div> {item.population}万人 </div>
+                                                </ResultList.Description>
+                                            </ResultList.Content>
+                                        </ResultList>
+                                    ))
+                                }
+                            </ResultListWrapper>
+                        )}
+                    </ReactiveList>
+                </ReactiveBase>
+            </div>
+        );
+    }
 }
 
 export default App;
